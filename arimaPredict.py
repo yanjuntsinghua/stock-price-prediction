192bf8d7ae7af3a658b6b8061e98117ac7f05218
Luddite <dingsf6@gmail.com>
2017-12-15 11:46:10 +0800

    add arima

---
+++ arimaPredict.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 15 11:28:20 2017
+
+@author: Luddite
+"""
+
+import tushare as ts
+
+class arimaPredict:
+    def __init__(self,code,date):
+        self.code=code;
+        self.date=date;
+        
+    
+    
